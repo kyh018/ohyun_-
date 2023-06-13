96945781fedc9d2acaada406dec947e283c5714d
kyh018 <109337296+kyh018@users.noreply.github.com>
2023-06-13 12:05:20 +0000

    django 두번째 수업

--- 코딩기초_4일차/sticky_memo/memo/admin.py
+++ 코딩기초_4일차/sticky_memo/memo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from memo.models import Memo
 
-# Register your models here.
+@admin.register(Memo)
+class MemoAdmin(admin.ModelAdmin):
+    list_display = ['title']
--- 코딩기초_4일차/sticky_memo/memo/views.py
+++ 코딩기초_4일차/sticky_memo/memo/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from memo.models import Memo
 
-# Create your views here.
+def get_all_memo(request):
+    memo_list = Memo.objects.all()
+    
+    context = {"memos": memo_list}
+
+    return render(request, "memo_list.html", context)
